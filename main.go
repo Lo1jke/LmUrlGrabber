f1f347c46ff2c18cdee90fb912d814fc4f981693
lo1jke <Lo1jke@gmail.com>
2022-09-29 22:46:31 +0300

    add create db and select example

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
 const url_LM = "https://liquimoly.ru/t_import.php?id=15"
 
-func main(){
-
+func main() {
 
 	response, err := http.Get(url_LM)
 	if err != nil {
@@ -25,30 +25,69 @@ func main(){
 		log.Fatal(err)
 	}
 
-	db, err := sql.Open("sqlite3", "./db/lm.db")
+	os.Remove("./db/urlsBrands.db")
+
+	_, err = os.Create("./db/urlsBrands.db") // Create SQLite file
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
+
+	db, _ := sql.Open("sqlite3", "./db/urlsBrands.db")
+	defer db.Close()
+	createTable(db)
 
 	var responseString = string(responseData)
 	responseStrings := strings.Split(responseString, "\n")
 	for i := 0; i < len(responseStrings); i++ {
 		if responseStrings[i] != "" {
 			oneString := strings.Split(responseStrings[i], ";")
-			//fmt.Println(oneString)
-			insertUrl(db, oneString[0], oneString[1], oneString[2])
+			//article;url;picture_url
+			insertUrl(db, oneString[0], "LM", oneString[1], oneString[2])
 		}
 	}
+
+	displayURLs(db)
 }
 
-func insertUrl(db *sql.DB, article string, url string, picture string) {
-	insertURLSQL := `INSERT INTO urlinfo(article, url, picture, created) VALUES (?, ?, ?, datetime('now'))`
+func insertUrl(db *sql.DB, article string, brand string, url string, picture string) {
+	insertURLSQL := `INSERT INTO urlinfo(article, brand, URL, picture) VALUES (?, ?, ?, ?)`
 	statement, err := db.Prepare(insertURLSQL)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	_, err = statement.Exec(article, url, picture)
+	_, err = statement.Exec(article, brand, url, picture)
 	if err != nil {
 		log.Fatalln(err.Error())
+	}
+}
+
+func createTable(db *sql.DB) {
+	createTableSQL := `CREATE TABLE urlinfo (
+		"id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"article" TEXT,
+		"brand" TEXT,		
+		"URL" TEXT,
+		"picture" TEXT		
+	  );`
+
+	statement, err := db.Prepare(createTableSQL) // Prepare SQL Statement
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec() // Execute SQL Statements
+}
+
+func displayURLs(db *sql.DB) {
+	row, err := db.Query("SELECT article, URL, picture FROM urlinfo WHERE brand = ? ORDER BY article", "LM")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() {
+		var article string
+		var url string
+		var picture string
+		row.Scan(&article, &url, &picture)
+		log.Printf("Article: %s URL: %s Picture: %s", article, url, picture)
 	}
 }
